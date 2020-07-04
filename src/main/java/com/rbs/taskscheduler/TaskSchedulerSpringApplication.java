3b8d4a16770136f973207dfb7ea336b2b893fc95
Ahmed AlSalih <Ahmed.Al-Salih@retailbusinessservices.com>
2020-07-04 01:43:19 -0400

    Spring Task Scheduler

---
+++ src/main/java/com/rbs/taskscheduler/TaskSchedulerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rbs.taskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbs/taskscheduler/TaskSchedulerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbs.taskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
